9e1992775af5a8fea88cc3d5becc2b0a74d42113
franco colapaolo <134982221+franncol@users.noreply.github.com>
2026-04-27 17:54:57 -0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,7 @@
+function abrirModal() {
+  document.getElementById("modal").classList.add("activo");
+}
+
+function cerrarModal() {
+  document.getElementById("modal").classList.remove("activo");
+}
